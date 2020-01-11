62533a92ec20baaa5862026b56251e9d1040b9b1
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-01-10 16:08:55 -0800

    Revert "adding pokemon reprository"
    This reverts commit 7a939a498503f733add8608b91a886a319073a7f.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,4 @@
-var repository = [];
-//this array will become the repository of Pokémon to display in my application
+alert('Hello World');
 
-var squirtle = {
-  name: 'Squirtle',
-  height: 0.5,
-  type: 'water',
-};
-
-var charmander = {
-  name: 'Charmander',
-  height: 0.6,
-  type: 'fire',
-};
-
-var venomoth = {
-  name: 'Venomoth',
-  height: 1.5,
-  type: 'bug, poison',
-};
-
-var jolteon = {
-  name: 'Jolteon',
-  height: 0.8,
-  type: 'electric',
-};
+var favoriteFood='sushi';
+document.write(favoriteFood);
