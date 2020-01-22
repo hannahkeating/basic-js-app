7e4adabac2279dad02deddd8da98914379a70028
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-01-22 13:38:20 -0800

    build repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
-var repository = ['squirtle', 'umbreon', 'venomoth', 'jolteon']{
+var repository = [
   {
     name: 'Squirtle',
     height: 0.5,
     type: ['water'],
-  }
+  },
   {
     name: 'Umbreon',
     height: 1,
     type: ['dark'],
-  }
+  },
   {
     name: 'Venomoth',
     height: 1.5,
     type: ['bug, poison'],
-  }{
+  },
+  {
     name: 'Jolteon',
     height: 0.8,
     type: ['electric'],
   }
-}
+]
 
-
-for( var i = 0; i < 5; i++){
-  if (i == 0){
-    document.write(i + height)
+for( var i = 0; i < 4; i++){
+  const pokemon = repository[i]
+  let message = `${pokemon.name} (height: ${pokemon.height})`
+  if (pokemon.height === 1.5) {
+    message = message + "-Wow, that's big!"
   }
+
+  document.write(`<div>${message}</div>`)
 }
