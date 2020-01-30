8b14fe0754cbd8474f48f041c9d023898d99f79d
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-01-29 17:44:25 -0800

    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,39 @@
-var repository = [
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    type: ['water'],
-  },
-  {
-    name: 'Umbreon',
-    height: 1,
-    type: ['dark'],
-  },
-  {
-    name: 'Venomoth',
-    height: 1.5,
-    type: ['bug, poison'],
-  },
-  {
-    name: 'Jolteon',
-    height: 0.8,
-    type: ['electric'],
+var pokemonRepository = (function(){
+  var repository = [
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      type: ['water'],
+    },
+    {
+      name: 'Umbreon',
+      height: 1,
+      type: ['dark'],
+    },
+    {
+      name: 'Venomoth',
+      height: 1.5,
+      type: ['bug, poison'],
+    },
+    {
+      name: 'Jolteon',
+      height: 0.8,
+      type: ['electric'],
+    }
+  ];
+  function getAll(pokemon) {
+    return repsitory;
   }
-]
+  function add(pokemon) {
+    repsitory.push(pokemon);
+  }
 
-repository.forEach(function(pokemon){
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.forEach(function getAll(pokemon){
   document.write(pokemon.name + ': ' + pokemon.height + ', ' + '(' + pokemon.type + ')' + '<br>');
 });
